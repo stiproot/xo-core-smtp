d36e9d9198a4be27c26d89c63148416411906855
stiproot <stipcich.simon@gmail.com>
2023-08-15 07:45:27 +0200

    chore:Lib update.

--- src/Xo.Core.Smtp/Usings.cs
+++ src/Xo.Core.Smtp/Usings.cs
@@ -5,6 +5,7 @@ global using Microsoft.Extensions.DependencyInjection.Extensions;
 global using Microsoft.Extensions.Options;
 global using MimeKit;
 global using Xo.Core.Abstractions;
+global using Xo.Core.Cqrs.Abstractions;
 global using Xo.Core.Extensions;
 global using Xo.Core.Smtp.Abstractions;
 global using Xo.Core.Smtp.Builders;
